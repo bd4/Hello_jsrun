65221db7e56c57f7454ba6fb3557f9566362a2ed
Thomas Papatheodore <papatheodore@ornl.gov>
2018-03-04 21:26:37 -0500

    changed some spaces to tabs in jsrun_layout.cu

--- jsrun_layout.cu
+++ jsrun_layout.cu
@@ -123,24 +123,24 @@ int main(int argc, char *argv[]){
 			sprintf(c_gpu_id, "%d", gpu_id);
 			if(i == 0){
 				strcpy(rt_gpu_id_list, strcat(c_i, " "));
-      	strcpy(gpu_id_list, strcat(c_gpu_id, " "));
+				strcpy(gpu_id_list, strcat(c_gpu_id, " "));
 				strncpy(uuid_list, uuid, 10*sizeof(char));
 				strcat(uuid_list, " ");
 				strcpy(busid_list, strcat(busid, " "));
 			}else{
 				strcat(rt_gpu_id_list, strcat(c_i, " "));
 				strcat(gpu_id_list, strcat(c_gpu_id, " "));
-        strcat(busid_list, strcat(busid, " "));
-        strncat(uuid_list, uuid, 10*sizeof(char));
-        strcat(uuid_list, " ");
+				strcat(busid_list, strcat(busid, " "));
+				strncat(uuid_list, uuid, 10*sizeof(char));
+				strcat(uuid_list, " ");
 			}
 
 		}
 
 		#pragma omp parallel default(shared) private(hwthread, thread_id)
 		{
-      thread_id = omp_get_thread_num();
-      hwthread = sched_getcpu();
+			thread_id = omp_get_thread_num();
+			hwthread = sched_getcpu();
 
 			if(strcmp(output_flag, "verbose") == 0){
 				printf("MPI Rank %03d, OMP_thread %02d on HWThread %03d of Node %s - RT_GPU_id %s: GPU_id %s, BusID %s, UUID %s\n", rank, thread_id, hwthread, name, rt_gpu_id_list, gpu_id_list, busid_list, uuid_list);
